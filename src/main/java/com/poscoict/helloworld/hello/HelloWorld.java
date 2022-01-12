aa8ab64346fc1f686de4f85ed5b3bebbb3b29a57
lee <ls4050@naver.com>
2022-01-12 10:22:49 +0900

    Hello World 수정

--- src/main/java/com/poscoict/helloworld/hello/HelloWorld.java
+++ src/main/java/com/poscoict/helloworld/hello/HelloWorld.java
@@ -1,7 +1,17 @@
 package com.poscoict.helloworld.hello;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class HelloWorld {
+	public static int count = 10;
 	public static void main(String[] args) {
+		hello();
 		System.out.println("Hello World!!!!!!!!");
+	}
+	
+	public static void hello() {
+		// block안에 있는 변수 = 지역변수 (stack에 저장)
+		Map<String, Object> m = new HashMap<>();
 	}
 }
